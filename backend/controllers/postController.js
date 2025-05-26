8335efbdd4a33ef8a3edeafc54733371740a1f92
AlexisGrumi <al.grumiz@gmail.com>
2025-05-26 02:39:43 -0400

    Finalizacion

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,21 +1,34 @@
 const { Post } = require('../models');
 
 exports.getAllPosts = async (req, res) => {
-  const posts = await Post.findAll();
-  res.json(posts);
+  try {
+    const posts = await Post.findAll();
+    res.json(posts);
+  } catch (error) {
+    res.status(300).json({ message: `Error - ${error.message}` })
+  }
 };
 
 exports.createPost = async (req, res) => {
-  const { name, description } = req.body;
-  const post = await Post.create({ name, description });
-  res.status(201).json(post);
+  try {
+    const { name, description } = req.body;
+    const post = await Post.create({ name, description });
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(300).json({ message: `Error - ${error.message}` })
+  }
 };
 
 exports.deletePost = async (req, res) => {
-  const post = await Post.findByPk(req.params.id);
-  if (!post) {
-    return res.status(404).json({ message: 'Post not found' });
+  try {
+    const post = await Post.findByPk(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+    await post.destroy();
+    res.json(post);
+  } catch (error) {
+    res.status(300).json({ message: `Error - ${error.message}` })
   }
-  await post.destroy();
-  res.json(post);
+
 };
